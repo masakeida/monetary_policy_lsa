7c01d0ba11279a4e6c4664cf4debd3e9d015343d
Masayuki Keida <masayuki@freebsd103.coildomain>
2016-11-20 23:43:06 +0900

    modify new mecab behavior: euc-jp to utf-8

--- mkFreq.sh
+++ mkFreq.sh
@@ -6,7 +6,7 @@ for file in ${file_list}
 do
     cp ${file}_fmt.txt input.txt
     ./morphological.py
-    nkf -u output.txt > ${file}_mecab.txt
+    cp output.txt ${file}_mecab.txt
 done
 
 rm input.txt output.txt
--- morphological.py
+++ morphological.py
@@ -3,17 +3,16 @@
 
 import sys
 reload(sys)
-sys.setdefaultencoding('euc-jp')
+sys.setdefaultencoding('utf-8')
 
 import codecs
 import MeCab
 
-#fin = codecs.open('input_e.txt', 'r', 'euc-jp')
 fin = codecs.open('input.txt', 'r', 'utf-8')
 text = fin.read()
 
 m = MeCab.Tagger("-Ochasen")
-parse = m.parse(text.encode('euc-jp', 'ignore'))
+parse = m.parse(text.encode('utf-8', 'ignore'))
 
-fout = codecs.open('output.txt', 'w', 'euc-jp')
+fout = codecs.open('output.txt', 'w', 'utf-8')
 fout.write(parse)
